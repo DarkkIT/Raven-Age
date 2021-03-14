28d03e1127e73bc0c7ccbe73fa68e140cd6498da
Darkk112 <50321264+Darkk112@users.noreply.github.com>
2021-03-14 14:53:41 +0200

    Add UserViewModel

---
+++ Web/RavenAge.Web.ViewModels/User/UserCurrencyViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace RavenAge.Web.ViewModels.User
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class UserCurrencyViewModel
+    {
+        public int Gold { get; set; } //// Premium
+
+        public int Silver { get; set; }
+
+        public int Stone { get; set; }
+
+        public int Wood { get; set; }
+    }
+}
