a91679ea5f2dd325e0508228316458034e88cc35
Darkk112 <50321264+Darkk112@users.noreply.github.com>
2021-03-25 18:15:10 +0200

    Add woodmine modal, service and controller

--- Web/RavenAge.Web/Startup.cs
+++ Web/RavenAge.Web/Startup.cs
@@ -25,6 +25,7 @@
     using RavenAge.Services.Data.HouseService;
     using RavenAge.Services.UserService.Data;
     using RavenAge.Services.Data.FarmService;
+    using RavenAge.Services.Data.SawMillService;
 
     public class Startup
     {
@@ -76,6 +77,7 @@
             services.AddTransient<IHouseService, HouseService>();
             services.AddTransient<IUserService,  UserService>();
             services.AddTransient<IFarmService, FarmService>();
+            services.AddTransient<ISawMillService, SawMillService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
