16bb71cf17e84a13ac32ff7e572e7e96977c8c4c
Hristoki <54570662+Hristoki@users.noreply.github.com>
2021-03-28 18:00:10 +0300

    Organize JS code in Scripts folder

---
+++ Web/RavenAge.Web/wwwroot/Scripts/Barracks.js
@@ -0,0 +1,44 @@
+﻿    $("input[value='Hire']").each(function (el) {
+        $(this).click(function () {
+
+            var input = $(this).prev().children('input');
+            var quantity = parseInt(input.val())
+            input.val('0');
+            var unit = input.attr('name');
+            var data = {};
+            data[`${unit}`] = quantity;
+            console.log(data);
+
+            var antiForgeryToken = $('input[name=__RequestVerificationToken]').val();
+
+            $.ajax({
+                type: "POST",
+                url: "/api/Barracks",
+                data: JSON.stringify(data),
+                dataType: 'json',
+                contentType: 'application/json',
+                headers: {
+                    'X-CSRF-TOKEN': antiForgeryToken
+                },
+
+                success: function (data) {
+
+                    $('span.resource').each(function (index) {
+                        var resource;
+                        if ($(this).text().includes('Silver')) {
+                            resource = (`Silver - ${Number($(this).text().split('Silver - ').filter(Boolean)) - data.silverSpent}`);
+                            $(this).text(resource);
+                        }
+                        else {
+                            resource = (`Wood - ${Number($(this).text().split('Wood - ').filter(Boolean)) - data.woodSpent}`);
+                            $(this).text(resource);
+                        }
+                    })
+
+                    var newUnitQuantity = Number($(`#${data.unitType}`).text().split(`${data.unitType} `).filter(Boolean)) + data.unitQuantity;
+                    $(`#${data.unitType}`).text(`${data.unitType} ${newUnitQuantity}`)
+                },
+            })
+
+        })
+    })
