5845dbed3e992be70017bbcaa94987b38d90d190
Hristoki <54570662+Hristoki@users.noreply.github.com>
2021-04-15 21:02:53 +0300

    Revert "Revert "Refactor Js UpdateResources""
    This reverts commit 2b40624ee967ae235f0a41d8a200f2b3d6c4c826.

--- Web/RavenAge.Web/wwwroot/Scripts/BarracksUpgrade.js
+++ Web/RavenAge.Web/wwwroot/Scripts/BarracksUpgrade.js
@@ -1,47 +1,35 @@
-﻿    $("input[value='Hire']").each(function (el) {
-        $(this).click(function () {
+﻿import { updateResource } from "./UpdateResource.js"
 
-            var input = $(this).prev().children('input');
-            var quantity = parseInt(input.val())
-            input.val('0');
-            var unit = input.attr('name');
-            var data = {};
-            data[`${unit}`] = quantity;
+$("input[value='Hire']").each(function (el) {
+    $(this).click(function () {
 
-            var antiForgeryToken = $('input[name=__RequestVerificationToken]').val();
+        var input = $(this).prev().children('input');
+        var quantity = parseInt(input.val())
+        input.val('0');
+        var unit = input.attr('name');
+        var data = {};
+        data[`${unit}`] = quantity;
 
-            $.ajax({
-                type: "POST",
-                url: "/api/Barracks",
-                data: JSON.stringify(data),
-                dataType: 'json',
-                contentType: 'application/json',
-                headers: {
-                    'X-CSRF-TOKEN': antiForgeryToken
-                },
-                
-                success: function (data) {
+        var antiForgeryToken = $('input[name=__RequestVerificationToken]').val();
 
-                    $('span.resource').each(function (index) {
-                        var resource;
-                        if ($(this).text().includes('Silver')) {
-                            resource = (`Silver - ${Number($(this).text().split('Silver - ').filter(Boolean)) - data.silverSpent}`);
-                            $(this).text(resource);
-                        }
-                        else if ($(this).text().includes('Stone')) {
-                            resource = (`Stone - ${Number($(this).text().split('Stone - ').filter(Boolean)) - data.stoneSpent}`);
-                            $(this).text(resource);
-                        }
-                        else {
-                            resource = (`Wood - ${Number($(this).text().split('Wood - ').filter(Boolean)) - data.woodSpent}`);
-                            $(this).text(resource);
-                        }
-                    })
+        $.ajax({
+            type: "POST",
+            url: "/api/Barracks",
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: 'application/json',
+            headers: {
+                'X-CSRF-TOKEN': antiForgeryToken
+            },
 
-                    var newUnitQuantity = Number($(`#${data.unitType}`).text().split(`${data.unitType} `).filter(Boolean)) + data.unitQuantity;
-                    $(`#${data.unitType}`).text(`${data.unitType} ${newUnitQuantity}`)
-                },
-            })
+            success: function (data) {
 
+                updateResource();
+
+                var newUnitQuantity = Number($(`#${data.unitType}`).text().split(`${data.unitType} `).filter(Boolean)) + data.unitQuantity;
+                $(`#${data.unitType}`).text(`${data.unitType} ${newUnitQuantity}`)
+            },
         })
+
     })
+})
