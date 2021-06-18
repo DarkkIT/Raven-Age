921d8d11dd6e6d8d951c56ff343b0ca6523c273b
DarkkIT <50321264+DarkkIT@users.noreply.github.com>
2021-06-18 19:57:25 +0300

    Update ArenaBattleService.cs

--- Services/RavenAge.Services.Data/ArenaBattleService/ArenaBattleService.cs
+++ Services/RavenAge.Services.Data/ArenaBattleService/ArenaBattleService.cs
@@ -31,61 +31,89 @@
         {
             var userCity = this.userCityRepo.All().FirstOrDefault(x => x.UserId == attackerId); //// Need attacker City Id !
 
-            var attackerCity = this.cityRepo.All().Where(x => x.Id == userCity.CityId).To<ArenaArmyDTO>().FirstOrDefault();
+            var attackerArmy = this.cityRepo.All().Where(x => x.Id == userCity.CityId).To<ArenaArmyDTO>().FirstOrDefault();
 
-            var defenderCity = this.cityRepo.All().Where(x => x.Id == defenderId).To<ArenaArmyDTO>().FirstOrDefault();
+            var defenderArmy = this.cityRepo.All().Where(x => x.Id == defenderId).To<ArenaArmyDTO>().FirstOrDefault();
 
             //// BattleLogic
 
-            var attackerArmyTotalCount = attackerCity.ArmyTotalCount;
-            var defenderArmyTotalCount = defenderCity.ArmyTotalCount;
+            var attackerArmyTotalCount = attackerArmy.ArmyTotalCount;
+            var defenderArmyTotalCount = defenderArmy.ArmyTotalCount;
 
-            while (attackerArmyTotalCount > 0 || defenderArmyTotalCount > 0)
+            while (attackerArmy.ArmyTotalCount > 0 || defenderArmy.ArmyTotalCount > 0)
             {
-                var attackerArchersCount = attackerCity.ArchersCount;
-                var attackerInfantryCount = attackerCity.InfantryCount;
-                var attackerCavaleryCount = attackerCity.CavaleryCount;
-                var attackerArtileryCount = attackerCity.ArtiletyCount;
+                var attackerArchersCount = attackerArmy.ArchersCount;
+                var attackerInfantryCount = attackerArmy.InfantryCount;
+                var attackerCavaleryCount = attackerArmy.CavaleryCount;
+                var attackerArtileryCount = attackerArmy.ArtiletyCount;
                 attackerArmyTotalCount = attackerArchersCount + attackerInfantryCount + attackerCavaleryCount + attackerArtileryCount;
 
-                var defenderArchersCount = defenderCity.ArchersCount;
-                var defenderInfantryCount = defenderCity.InfantryCount;
-                var defenderCavaleryCount = defenderCity.CavaleryCount;
-                var defenderArtileryCount = defenderCity.ArtiletyCount;
+                var defenderArchersCount = defenderArmy.ArchersCount;
+                var defenderInfantryCount = defenderArmy.InfantryCount;
+                var defenderCavaleryCount = defenderArmy.CavaleryCount;
+                var defenderArtileryCount = defenderArmy.ArtiletyCount;
                 defenderArmyTotalCount = defenderArchersCount + defenderInfantryCount + defenderCavaleryCount + defenderArtileryCount;
 
                 //// Phase 1 - Artilery attack enemy Artilery, then attack enemy Archers
 
-                var defenderArtileryHealtLeft =
-                    (defenderCity.FullArtileryHealth * defenderArtileryCount)
-                    - (attackerCity.FullArtileryAttack * attackerArtileryCount);
+                defenderArtileryCount = this.UnitAttack(defenderArmy.FullArtileryHealth * defenderArtileryCount, attackerArmy.FullArtileryAttack * attackerArtileryCount, defenderArmy.FullArtileryHealth);
 
-                defenderArtileryCount = defenderArtileryHealtLeft / defenderCity.FullArtileryHealth;
+                defenderArchersCount = this.UnitAttack(defenderArmy.FullArcherHealth * defenderArchersCount, attackerArmy.FullArtileryAttack * attackerArtileryCount, defenderArmy.FullArcherHealth);
 
-                var defenderArchersHeltLeft =
-                    (defenderCity.FullArcherHealth * defenderArchersCount)
-                    - (attackerCity.FullArtileryAttack * attackerArtileryCount);
+                //// Phase 1.2 - enemy Artilery responds with the same attack
 
-                defenderArchersCount = defenderArchersHeltLeft / defenderCity.FullArcherHealth;
+                attackerArtileryCount = this.UnitAttack(attackerArmy.FullArtileryHealth * attackerArtileryCount, defenderArmy.FullArtileryAttack * defenderArtileryCount, attackerArmy.FullArtileryHealth);
 
-                //// Phase 2 - enemy Artilery responds with the same attack
+                attackerArchersCount = this.UnitAttack(attackerArmy.FullArcherHealth * attackerArchersCount, defenderArmy.FullArtileryAttack * defenderArtileryCount, attackerArmy.FullArcherHealth);
 
-                var attackerArtileryHelthLeft =
-                    (attackerCity.FullArtileryHealth * attackerArtileryCount)
-                    - (defenderCity.FullArtileryAttack * defenderArtileryCount);
+                //// Phase 2 - Archers attack enemy Infantry, then attack enemy Cavalery
 
-                attackerArtileryCount = attackerArtileryHelthLeft / attackerCity.FullArtileryHealth;
+                defenderInfantryCount = this.UnitAttack(defenderArmy.FullInfantryHealth * defenderInfantryCount, attackerArmy.FullArcherAttack * attackerArchersCount, defenderArmy.FullInfantryHealth);
 
-                var attackerArchersHealthLeft =
-                    (attackerCity.FullArcherHealth * attackerArchersCount)
-                    - (defenderCity.FullArtileryAttack * defenderArtileryCount);
+                defenderCavaleryCount = this.UnitAttack(defenderArmy.FullCavaleryHealth * defenderCavaleryCount, attackerArmy.FullArcherAttack * attackerArchersCount, defenderArmy.FullCavaleryHealth);
 
-                attackerArchersCount = attackerArchersHealthLeft / attackerCity.FullArcherHealth;
+                //// Phase 2.2 - enemy Archars responds with the same attack
+
+                attackerInfantryCount = this.UnitAttack(attackerArmy.FullInfantryHealth * attackerInfantryCount, defenderArmy.FullArcherAttack * defenderArchersCount, attackerArmy.FullInfantryHealth);
+
+                attackerCavaleryCount = this.UnitAttack(attackerArmy.FullCavaleryHealth * attackerCavaleryCount, defenderArmy.FullArcherAttack * defenderArchersCount, attackerArmy.FullCavaleryHealth);
+
+                //// Phase 3 - Cavalery attack enemy Infantry, then attack enemy Cavalery
+
+                defenderInfantryCount = this.UnitAttack(defenderArmy.FullInfantryHealth * defenderInfantryCount, attackerArmy.FullCavaleryAttack * attackerCavaleryCount, defenderArmy.FullInfantryHealth);
+
+                defenderCavaleryCount = this.UnitAttack(defenderArmy.FullCavaleryHealth * defenderCavaleryCount, attackerArmy.FullCavaleryAttack * attackerCavaleryCount, defenderArmy.FullCavaleryHealth);
+
+                //// Phase 3.2 - enemy Cavalery responds with the same attack
+
+                attackerInfantryCount = this.UnitAttack(attackerArmy.FullInfantryHealth * attackerInfantryCount, defenderArmy.FullCavaleryAttack * defenderCavaleryCount, attackerArmy.FullInfantryHealth);
+
+                attackerCavaleryCount = this.UnitAttack(attackerArmy.FullCavaleryHealth * attackerCavaleryCount, defenderArmy.FullCavaleryAttack * defenderCavaleryCount, attackerArmy.FullCavaleryHealth);
+
+                //// Phase 4 - Infantry attack enemy Cvalery , then attack enemy Infantry
+
+                defenderCavaleryCount = this.UnitAttack(defenderArmy.FullCavaleryHealth * defenderCavaleryCount, attackerArmy.FullInfantriAttack * attackerInfantryCount, defenderArmy.FullCavaleryHealth);
+
+                defenderInfantryCount = this.UnitAttack(defenderArmy.FullInfantryHealth * defenderInfantryCount, attackerArmy.FullInfantriAttack * attackerInfantryCount, defenderArmy.FullInfantryHealth);
+
+                //// Phase 4.2 - enemy Infantry responds with the same attack
+
+                attackerCavaleryCount = this.UnitAttack(attackerArmy.FullCavaleryHealth * attackerCavaleryCount, defenderArmy.FullInfantriAttack * defenderInfantryCount, attackerArmy.FullCavaleryHealth);
+
+                attackerInfantryCount = this.UnitAttack(attackerArmy.FullInfantryHealth * attackerInfantryCount, defenderArmy.FullInfantriAttack * defenderInfantryCount, attackerArmy.FullInfantryHealth);
             }
 
             var model = new BattleResultViewModel { ArenaPoints = 15 };
 
             return model;
+        }
+
+        public int UnitAttack(int totalUnitHealth, int totalUnitAttack, int singleUnitHealth)
+        {
+            var defenderUnitHealtLeft =
+                    totalUnitHealth - totalUnitAttack;
+
+            return defenderUnitHealtLeft / singleUnitHealth;
         }
     }
 }
